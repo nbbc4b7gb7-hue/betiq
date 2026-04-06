c551baa6390207b9b0ab3c278a62da09f31bb746
nbbc4b7gb7-hue <alexfortier10@gmail.com>
2026-04-06 16:36:29 -0400

    Initialize main.jsx with React and BetIQ component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BetIQ from "./BetIQ.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BetIQ />
+  </React.StrictMode>
+);
